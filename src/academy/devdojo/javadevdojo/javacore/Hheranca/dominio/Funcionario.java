08f534775b79862dbfda0173345db700226f7050
rickxyo <pedrinhooldoni@gmail.com>
2025-01-29 21:32:52 -0300

    study: herancas 03

--- src/academy/devdojo/javadevdojo/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/javadevdojo/javacore/Hheranca/dominio/Funcionario.java
@@ -1,11 +1,15 @@
 package academy.devdojo.javadevdojo.javacore.Hheranca.dominio;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
     private double salario;
 
-    public void imprime(){
+    public void imprime() {
         super.imprime();
         System.out.println(this.salario);
+    }
+
+    public void relatorioPagamento() {
+        System.out.println("Eu " + this.name + " recebi o salario de " + this.salario);
     }
 
     public double getSalario() {
--- src/academy/devdojo/javadevdojo/javacore/Hheranca/dominio/Pessoa.java
+++ src/academy/devdojo/javadevdojo/javacore/Hheranca/dominio/Pessoa.java
@@ -1,9 +1,9 @@
 package academy.devdojo.javadevdojo.javacore.Hheranca.dominio;
 
 public class Pessoa {
-    private String name;
-    private String cpf;
-    private Endereco endereco;
+    protected String name;
+    protected String cpf;
+    protected Endereco endereco;
 
     public void imprime() {
         System.out.println(this.name);
