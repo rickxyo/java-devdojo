4b25dca90ac00bd291391c4139ec407eebfe439b
rickxyo <pedrinhooldoni@gmail.com>
2025-01-30 20:54:15 -0300

    study: metodo toString

---
+++ src/academy/devdojo/javadevdojo/javacore/Isobrescrita/dominio/Anime.java
@@ -0,0 +1,24 @@
+package academy.devdojo.javadevdojo.javacore.Isobrescrita.dominio;
+
+public class Anime extends Object {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/academy/devdojo/javadevdojo/javacore/Isobrescrita/test/AnimeTest1.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javadevdojo.javacore.Isobrescrita.test;
+
+import academy.devdojo.javadevdojo.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest1 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
