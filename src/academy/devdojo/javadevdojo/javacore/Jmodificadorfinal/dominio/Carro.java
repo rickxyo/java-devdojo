1f3b34cf27d1bad93c0ef38e86306decb552fa2b
rickxyo <pedrinhooldoni@gmail.com>
2025-02-01 23:27:50 -0300

    study: modificador final 02

--- src/academy/devdojo/javadevdojo/javacore/Jmodificadorfinal/dominio/Carro.java
+++ src/academy/devdojo/javadevdojo/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -3,6 +3,7 @@ package academy.devdojo.javadevdojo.javacore.Jmodificadorfinal.dominio;
 public class Carro {
     private String name;
     public static final double VELOCIDADE_LIMITE = 250;
+    public final Comprador COMPRADOR = new Comprador();
 
     public String getName() {
         return name;
