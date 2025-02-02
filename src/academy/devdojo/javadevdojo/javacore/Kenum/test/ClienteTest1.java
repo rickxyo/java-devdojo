4b57e8ca79a4b31a2dc1f4e3919f8f17c576acf2
rickxyo <pedrinhooldoni@gmail.com>
2025-02-02 10:02:16 -0300

    study: enumeracao 02

--- src/academy/devdojo/javadevdojo/javacore/Kenum/test/ClienteTest1.java
+++ src/academy/devdojo/javadevdojo/javacore/Kenum/test/ClienteTest1.java
@@ -1,17 +1,15 @@
 package academy.devdojo.javadevdojo.javacore.Kenum.test;
 
 import academy.devdojo.javadevdojo.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.javadevdojo.javacore.Kenum.dominio.Cliente.TipoPagamento;
 import academy.devdojo.javadevdojo.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest1 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
     }
 }
