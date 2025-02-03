76dcf6c0ec1c7c7fbae52fa3a48a3f104fb71041
rickxyo <pedrinhooldoni@gmail.com>
2025-02-03 00:32:00 -0300

    study: enumeracao 04

--- src/academy/devdojo/javadevdojo/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/javadevdojo/javacore/Kenum/dominio/TipoCliente.java
@@ -6,10 +6,18 @@ public enum TipoCliente {
 
     private int valor;
     private String nomeRelatorio;
-
     TipoCliente(int valor, String nomeRelatorio) {
         this.valor = valor;
         this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio){
+        for (TipoCliente tipoCliente : values()) {
+            if (tipoCliente.getNomeRelatorio().equals(nomeRelatorio)){
+                return  tipoCliente;
+            }
+        }
+        return null;
     }
 
     public int getValor() {
--- src/academy/devdojo/javadevdojo/javacore/Kenum/test/ClienteTest1.java
+++ src/academy/devdojo/javadevdojo/javacore/Kenum/test/ClienteTest1.java
@@ -14,5 +14,10 @@ public class ClienteTest1 {
 
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica");
+        System.out.println(tipoCliente2);
     }
 }
