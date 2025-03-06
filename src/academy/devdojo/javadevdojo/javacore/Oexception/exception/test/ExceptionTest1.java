f85bc12a827b3afc70f97f7f51c486de379052d0
rickxyo <pedrinhooldoni@gmail.com>
2025-03-05 22:20:21 -0300

    study: exceções 03

---
+++ src/academy/devdojo/javadevdojo/javacore/Oexception/exception/test/ExceptionTest1.java
@@ -0,0 +1,22 @@
+package academy.devdojo.javadevdojo.javacore.Oexception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest1 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
