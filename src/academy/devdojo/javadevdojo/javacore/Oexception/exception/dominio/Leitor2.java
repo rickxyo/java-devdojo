4a5cb9c6f58bc6386052f8506cc945b6c8671b4a
rickxyo <pedrinhooldoni@gmail.com>
2025-03-10 11:08:41 -0300

    study: exceções 09

---
+++ src/academy/devdojo/javadevdojo/javacore/Oexception/exception/dominio/Leitor2.java
@@ -0,0 +1,13 @@
+package academy.devdojo.javadevdojo.javacore.Oexception.exception.dominio;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Leitor2 implements Closeable {
+    @Override
+    public void close() throws IOException {
+        System.out.println("Fechando leitor 2");
+    }
+}
+
+
---
+++ src/academy/devdojo/javadevdojo/javacore/Oexception/exception/test/TryWithResourcesTest1.java
@@ -0,0 +1,37 @@
+package academy.devdojo.javadevdojo.javacore.Oexception.exception.test;
+
+import academy.devdojo.javadevdojo.javacore.Oexception.exception.dominio.Leitor1;
+import academy.devdojo.javadevdojo.javacore.Oexception.exception.dominio.Leitor2;
+
+import java.io.*;
+
+public class TryWithResourcesTest1 {
+    public static void main(String[] args) {
+        lerArquivo();
+    }
+
+    public static void lerArquivo() {
+        try (Leitor1 leitor1 = new Leitor1();
+             Leitor2 leitor2 = new Leitor2()) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void lerArquivo2() {
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("test.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
