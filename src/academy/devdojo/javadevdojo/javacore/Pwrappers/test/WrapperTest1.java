8c4b170d66983dac4ac4e9a7c2a61ae4c8d3e5ab
rickxyo <pedrinhooldoni@gmail.com>
2025-03-12 10:19:40 -0300

    study: wrappers 01

---
+++ src/academy/devdojo/javadevdojo/javacore/Pwrappers/test/WrapperTest1.java
@@ -0,0 +1,25 @@
+package academy.devdojo.javadevdojo.javacore.Pwrappers.test;
+
+public class WrapperTest1 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 10;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 10;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+
+    }
+}
