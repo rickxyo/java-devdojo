830c8d6eaea151a994aa3f5d631def9fd0e33a78
rickxyo <pedrinhooldoni@gmail.com>
2025-03-12 11:12:04 -0300

    study: strings 01

---
+++ src/academy/devdojo/javadevdojo/javacore/Qstring/test/StringTest1.java
@@ -0,0 +1,15 @@
+package academy.devdojo.javadevdojo.javacore.Qstring.test;
+
+public class StringTest1 {
+    public static void main(String[] args) {
+        // strings são IMUTÁVEIS
+        String nome = "Pedro"; // String constant pool
+        String nome2 = "Pedro";
+        nome = nome.concat(" Henrique");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("Pedro"); // 1 variavel de referencia, 2 objeto do tipo string, 3 uma string no pool de string
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());
+    }
+}
