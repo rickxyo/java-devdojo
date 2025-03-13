d51cfc1d3de3ab466dafee2327796a98cb249993
rickxyo <pedrinhooldoni@gmail.com>
2025-03-13 16:28:24 -0300

    study: date

---
+++ src/academy/devdojo/javadevdojo/javacore/Rdates/test/DateTest1.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javadevdojo.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest1 {
+    public static void main(String[] args) {
+        Date date = new Date(1741893546095L); // long 100000
+        System.out.println(date);
+    }
+}
