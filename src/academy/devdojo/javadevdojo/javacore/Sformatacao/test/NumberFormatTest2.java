f3c2b86587294d5d011e3126a519fe0914c130d5
rickxyo <pedrinhooldoni@gmail.com>
2025-03-17 10:27:04 -0300

    classes utilitarias: internacionalização moeda com locale

--- src/academy/devdojo/javadevdojo/javacore/Sformatacao/test/NumberFormatTest2.java
+++ src/academy/devdojo/javadevdojo/javacore/Sformatacao/test/NumberFormatTest2.java
@@ -4,21 +4,21 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest1 {
+public class NumberFormatTest2 {
     public static void main(String[] args) {
         Locale localeDefault = Locale.getDefault();
         Locale localeBR = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(localeBR);
-        nfa[3] = NumberFormat.getInstance(localeIT);
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
         double valor = 1_000.2130;
         for (NumberFormat numberFormat : nfa) {
             //System.out.println(numberFormat.getMaximumFractionDigits());
-            numberFormat.setMaximumFractionDigits(2);
+            System.out.println(numberFormat.getMaximumFractionDigits());
             System.out.println(numberFormat.format(valor));
         }
         String valorString = "1_000.2130";
