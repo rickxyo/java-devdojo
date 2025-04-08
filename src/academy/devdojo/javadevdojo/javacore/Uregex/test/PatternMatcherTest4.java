453f0e98b9220b09818d9c704b4ee364c2b69552
rickxyo <pedrinhooldoni@gmail.com>
2025-04-07 23:31:03 -0300

    classes utilitarias: Regex 04 Pattern e Matcher - Quantificadores

--- src/academy/devdojo/javadevdojo/javacore/Uregex/test/PatternMatcherTest4.java
+++ src/academy/devdojo/javadevdojo/javacore/Uregex/test/PatternMatcherTest4.java
@@ -3,7 +3,7 @@ package academy.devdojo.javadevdojo.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest3 {
+public class PatternMatcherTest4 {
     public static void main(String[] args) {
         // \d = todos os digitos
         // \D = tudo o que NÃO for digito
@@ -12,12 +12,17 @@ public class PatternMatcherTest3 {
         // \w = a-zA-Z, digitos, _
         // \W = tudo que NÃO for incluso no \w
         // []
-        String regex = "0[xX][0-9a-fA-F]";
-        //String texto = "abaaba";
-        String texto2 = "12 0x 0X 0xFFABC 0x109 0x1";
+        // ? Zero ou uma
+        // * Zero ou mais
+        // + Uma ou mais
+        // {n, m} de n até m
+        // ()
+        // $
+        String regex = "0[xX]([0-9a-fA-F])+\\s";
+        String texto = "12 0x 0X 0xFFABC 0x10G 0x1";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(texto2);
-        System.out.println("texto:  "+texto2);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:  "+texto);
         System.out.println("indice: 0123456789");
         System.out.println("regex "+regex);
         System.out.println("Posicoes encontradas");
