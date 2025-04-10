0be1b578eaeb435cbb11a2659b1f4697e4044b76
rickxyo <pedrinhooldoni@gmail.com>
2025-04-09 22:35:41 -0300

    classes utilitarias: Scanner - Tokens e Delimitadores

---
+++ src/academy/devdojo/javadevdojo/javacore/Uregex/test/ScannerTest2.java
@@ -0,0 +1,23 @@
+package academy.devdojo.javadevdojo.javacore.Uregex.test;
+
+import java.util.Scanner;
+
+public class ScannerTest2 {
+    public static void main(String[] args) {
+        String texto = "Levi,Eren,Mikasa,true,200";
+        Scanner scanner = new Scanner(texto);
+        scanner.useDelimiter(",");
+        while (scanner.hasNext()) {
+            //System.out.println(scanner.next());
+            if (scanner.hasNextInt()) {
+                int i = scanner.nextInt();
+                System.out.println("Int " + i);
+            } else if (scanner.hasNextBoolean()) {
+                boolean b = scanner.nextBoolean();
+                System.out.println("Boolean " + b);
+            } else {
+                System.out.println(scanner.next());
+            }
+        }
+    }
+}
