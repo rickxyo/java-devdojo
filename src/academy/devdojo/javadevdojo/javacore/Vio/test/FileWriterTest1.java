571e45bd9bd05a08c2147c2456e23db4a4d73b5a
rickxyo <pedrinhooldoni@gmail.com>
2025-04-11 22:08:35 -0300

    IO 03 - FileReader

--- src/academy/devdojo/javadevdojo/javacore/Vio/test/FileWriterTest1.java
+++ src/academy/devdojo/javadevdojo/javacore/Vio/test/FileWriterTest1.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileWriter1 {
+public class FileWriterTest1 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file)){
