3f15042fbffdeb37a73db00ca3db9ce7b80ab419
rickxyo <pedrinhooldoni@gmail.com>
2025-04-11 21:55:35 -0300

    classes utilitarias: IO 02 - FileWriter

---
+++ src/academy/devdojo/javadevdojo/javacore/Vio/test/FileWriter1.java
@@ -0,0 +1,17 @@
+package academy.devdojo.javadevdojo.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter1 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file)){
+            fw.write("O DEVDOJO É O MELHOR CURSO DO BRASIL");
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
