a82f7f98d21bba71f9119cb840292f5375c34210
rickxyo <pedrinhooldoni@gmail.com>
2025-04-13 00:13:01 -0300

    classes utilitarias: IO 05 - BufferedReader

--- src/academy/devdojo/javadevdojo/javacore/Vio/test/BufferedWriterTest1.java
+++ src/academy/devdojo/javadevdojo/javacore/Vio/test/BufferedWriterTest1.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest1 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)){
-            br.write("O DEVDOJO É O MELHOR CURSO DO BRASIL");
-            br.newLine();
-            br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)){
+            bw.write("O DEVDOJO É O MELHOR CURSO DO BRASIL");
+            bw.newLine();
+            bw.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
