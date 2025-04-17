2f30a4321bfea91301314b4fc53a2e74dff498c7
rickxyo <pedrinhooldoni@gmail.com>
2025-04-16 22:17:52 -0300

    classes utilitarias: NIO 06 - BasicFileAttributes

---
+++ src/academy/devdojo/javadevdojo/javacore/Wnio/test/BasicFileAttributesTest1.java
@@ -0,0 +1,25 @@
+package academy.devdojo.javadevdojo.javacore.Wnio.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class BasicFileAttributesTest1 {
+    public static void main(String[] args) throws IOException {
+//        BasicFileAttributes, DosFileAttributes,  PosixFileAttributes
+        LocalDateTime date = LocalDateTime.now().minusDays(10);
+        File file = new File("pasta/novo.txt");
+        boolean isCreated = file.createNewFile();
+        boolean isModified = file.setLastModified(date.toInstant(ZoneOffset.UTC).toEpochMilli());
+
+        Path path = Paths.get("pasta/novo_path.txt");
+        Files.createFile(path);
+        FileTime fileTime = FileTime.from(date.toInstant(ZoneOffset.UTC));
+        Files.setLastModifiedTime(path, fileTime);
+    }
+}
