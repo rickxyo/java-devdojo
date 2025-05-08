0598881a776736439e973f14622d7e84f5aa4eb5
rickxyo <pedrinhooldoni@gmail.com>
2025-05-07 21:53:31 -0300

    colecoes: conversão de lista para arrays e vice versa

---
+++ src/academy/devdojo/javadevdojo/javacore/Ycolecoes/test/ListaArrayConversaoTest1.java
@@ -0,0 +1,33 @@
+package academy.devdojo.javadevdojo.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListaArrayConversaoTest1 {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        Integer[] integersArray = numeros.toArray(new Integer[0]);
+        System.out.println(Arrays.toString(integersArray));
+
+        System.out.println("----------------");
+
+        Integer[] numerosArray = new Integer[3];
+        numerosArray[0] = 1;
+        numerosArray[1] = 2;
+        numerosArray[2] = 3;
+        List<Integer> arrayToList = Arrays.asList(numerosArray);
+        System.out.println(Arrays.toString(numerosArray));
+        System.out.println(arrayToList);
+
+        System.out.println("----------------");
+
+        List<Integer> numerosList = new ArrayList<>(Arrays.asList(numerosArray));
+        numerosList.add(15);
+        System.out.println(numerosList);
+
+    }
+}
