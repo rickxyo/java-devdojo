dcd81a5d00729e74c131357dc1fca19d95ca9621
rickxyo <pedrinhooldoni@gmail.com>
2025-05-20 22:54:34 -0300

    colecoes: NavigableMap, TreeMap

---
+++ src/academy/devdojo/javadevdojo/javacore/Ycolecoes/test/NavigableMapTest1.java
@@ -0,0 +1,20 @@
+package academy.devdojo.javadevdojo.javacore.Ycolecoes.test;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NavigableMapTest1 {
+    public static void main(String[] args) {
+        NavigableMap<String, String> map = new TreeMap<>();
+        map.put("A", "Letra A");
+        map.put("D", "Letra D");
+        map.put("B", "Letra B");
+        map.put("C", "Letra C");
+        map.put("E", "Letra E");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
