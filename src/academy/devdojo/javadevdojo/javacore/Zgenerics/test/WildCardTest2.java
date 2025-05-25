8113e1dbbc05e072d6e83aff445d07ca43b8b1d5
rickxyo <pedrinhooldoni@gmail.com>
2025-05-25 10:34:33 -0300

    generics: WildCard 02

--- src/academy/devdojo/javadevdojo/javacore/Zgenerics/test/WildCardTest2.java
+++ src/academy/devdojo/javadevdojo/javacore/Zgenerics/test/WildCardTest2.java
@@ -7,12 +7,20 @@ public class WildCardTest2 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatos);
     }
 
-    private static void printConsulta(List<Animal> animals){
+    // AQUI VOCE ESTÁ FALANDO QUE PODE RECEBER QUALQUER UM QUE SEJA "Animal" OU QUALQUER FILHO
+    private static void printConsulta(List<? extends Animal> animals){
         for (Animal animal : animals) {
             animal.consulta();
         }
+    }
 
+    // AQUI VOCE ESTÁ FALANDO QUE PODE RECEBER QUALQUER UM QUE SEJA "Animal" OU QUALQUER PAI
+    private static void printConsultaAnimal(List<? super Animal> animals){
+        animals.add(new Cachorro());
+        animals.add(new Gato());
     }
 }
