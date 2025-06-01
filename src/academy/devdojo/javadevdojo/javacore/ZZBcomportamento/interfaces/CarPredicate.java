458e76d765a8a7d1a91625db345f47fbb5382e47
rickxyo <pedrinhooldoni@gmail.com>
2025-05-31 23:27:06 -0300

    lambdas: predicate

--- src/academy/devdojo/javadevdojo/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/javadevdojo/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,15 @@ package academy.devdojo.javadevdojo.javacore.ZZBcomportamento.interfaces;
 
 import academy.devdojo.javadevdojo.javacore.ZZBcomportamento.domain.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    //PARA UTILIZAR LAMBDAS PRECISAMOS DE UMA INTERFACE FUNCIONAL
+    //PARA SER CONSIDERADO UMA INTERFACE FUNCIONAL, PRECISA TER APENAS E NO MAXIMO 1 METODO ABSTRATO
+
+    //anonimas, funcoes, conciso
     boolean test(Car car);
+    //(parametro) -> <expressao>
+    //(Car car) -> car.getColor().equals(green);
+
+
 }
