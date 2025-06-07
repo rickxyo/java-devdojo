ca6a1ef3519ddbc4112385836079d13ea4017d3d
rickxyo <pedrinhooldoni@gmail.com>
2025-06-06 23:17:35 -0300

    streams: introduction 03

--- src/academy/devdojo/javadevdojo/javacore/ZZEstreams/domain/LightNovel.java
+++ src/academy/devdojo/javadevdojo/javacore/ZZEstreams/domain/LightNovel.java
@@ -1,5 +1,7 @@
 package academy.devdojo.javadevdojo.javacore.ZZEstreams.domain;
 
+import java.util.Objects;
+
 public class LightNovel {
     private String title;
     private double price;
@@ -15,6 +17,18 @@ public class LightNovel {
                 "title='" + title + '\'' +
                 ", price=" + price +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title);
     }
 
     public String getTitle() {
