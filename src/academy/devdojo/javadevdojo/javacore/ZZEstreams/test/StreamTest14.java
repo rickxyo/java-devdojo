db350dcb2e4a61ff3a80a8e8ebe2ff8da86808cc
rickxyo <pedrinhooldoni@gmail.com>
2025-06-21 23:12:37 -0300

    streams: collectors 04

---
+++ src/academy/devdojo/javadevdojo/javacore/ZZEstreams/test/StreamTest14.java
@@ -0,0 +1,45 @@
+package academy.devdojo.javadevdojo.javacore.ZZEstreams.test;
+
+import academy.devdojo.javadevdojo.javacore.ZZEstreams.domain.Category;
+import academy.devdojo.javadevdojo.javacore.ZZEstreams.domain.LightNovel;
+import academy.devdojo.javadevdojo.javacore.ZZEstreams.domain.Promotion;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class StreamTest14 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlord", 10.99, Category.FANTASY),
+            new LightNovel("Violet Evergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no life", 2.99, Category.FANTASY),
+            new LightNovel("Fullmetal Alchemist", 5.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Monogatari", 4.00, Category.ROMANCE)
+    ));
+
+    public static void main(String[] args) {
+        //LISTANDO A QUANTIDADE DE CADA CATEGORIA
+        //{FANTASY=6, DRAMA=1, ROMANCE=1}
+        Map<Category, Long> collect = lightNovels.stream().collect(Collectors.groupingBy(LightNovel::getCategory, Collectors.counting()));
+        System.out.println(collect);
+
+        //LISTANDO A LIGHTNOVEL MAIS CARE DE CADA CATEGORIA
+        Map<Category, Optional<LightNovel>> collect1 = lightNovels.stream()
+                .collect(Collectors.groupingBy(LightNovel::getCategory, Collectors.maxBy(Comparator.comparing(LightNovel::getPrice))));
+        System.out.println(collect1);
+
+        //LISTANDO A LIGHTNOVEL MAIS CARE DE CADA CATEGORIA SEM O "Optional"
+        Map<Category, LightNovel> collect2 = lightNovels.stream().collect(Collectors.groupingBy(LightNovel::getCategory,
+                Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(LightNovel::getPrice)), Optional::get)));
+        System.out.println(collect2);
+
+        //MESMO RESULTADO QUE O CODIGO ACIMA
+        Map<Category, LightNovel> collect3 = lightNovels.stream().collect(Collectors.toMap(LightNovel::getCategory, Function.identity(), BinaryOperator.maxBy(Comparator.comparing(LightNovel::getPrice))));
+        System.out.println(collect3);
+
+    }
+}
