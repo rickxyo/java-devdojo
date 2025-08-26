88b7e0cc2770a24b6c96f866674f93b72d3e218f
rickxyo <pedrinhooldoni@gmail.com>
2025-08-25 21:23:04 -0300

    threads: prioridade e sleep

--- src/academy/devdojo/javadevdojo/javacore/ZZFthreads/test/ThreadTest1.java
+++ src/academy/devdojo/javadevdojo/javacore/ZZFthreads/test/ThreadTest1.java
@@ -34,21 +34,28 @@ class ThreadExampleRunnable implements Runnable{
             if (i % 100 == 0){
                 System.out.println();
             }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 // Daemon x User
 public class ThreadTest1 {
     // pode ser um objeto ou um processo / linha de execução
+    // toda as vezes que iniciamos o projeto, a thread main é executada
     public static void main(String[] args) {
 //        ThreadExemple t1 = new ThreadExemple('A');
 //        ThreadExemple t2 = new ThreadExemple('B');
 //        ThreadExemple t3 = new ThreadExemple('C');
 //        ThreadExemple t4 = new ThreadExemple('D');
-        Thread t1 = new Thread(new ThreadExemple('A'));
-        Thread t2 = new Thread(new ThreadExemple('B'));
-        Thread t3 = new Thread(new ThreadExemple('C'));
-        Thread t4 = new Thread(new ThreadExemple('D'));
+        Thread t1 = new Thread(new ThreadExemple('A'), "T1");
+        Thread t2 = new Thread(new ThreadExemple('B'), "T2");
+        Thread t3 = new Thread(new ThreadExemple('C'), "T3");
+        Thread t4 = new Thread(new ThreadExemple('D'), "T4");
+        t4.setPriority(Thread.MAX_PRIORITY);
         t1.start();
         t2.start();
         t3.start();
