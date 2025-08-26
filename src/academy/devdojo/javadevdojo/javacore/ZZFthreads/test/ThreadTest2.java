a61575f23920922e70bff9522788667ce105688f
rickxyo <pedrinhooldoni@gmail.com>
2025-08-25 21:52:15 -0300

    threads: yield e join

---
+++ src/academy/devdojo/javadevdojo/javacore/ZZFthreads/test/ThreadTest2.java
@@ -0,0 +1,34 @@
+package academy.devdojo.javadevdojo.javacore.ZZFthreads.test;
+
+
+
+class ThreadExampleRunnable2 implements Runnable{
+    private final String c;
+
+    public ThreadExampleRunnable2(String c){
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0){
+                System.out.println();
+            }
+            Thread.yield();
+        }
+    }
+}
+
+public class ThreadTest2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ThreadExampleRunnable2("KA"));
+        Thread t2 = new Thread(new ThreadExampleRunnable2("ME"));
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t1.start();
+        t1.join();
+        t2.start();
+    }
+}
