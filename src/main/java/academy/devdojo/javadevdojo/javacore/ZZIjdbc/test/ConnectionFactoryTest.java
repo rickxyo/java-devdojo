68e5754f81a936fe36650a6446b83a9a800d3023
rickxyo <pedrinhooldoni@gmail.com>
2025-10-26 01:04:09 -0300

    jdbc: adicionando dependências + conectando banco

---
+++ src/main/java/academy/devdojo/javadevdojo/javacore/ZZIjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package academy.devdojo.javadevdojo.javacore.ZZIjdbc.test;
+
+import academy.devdojo.javadevdojo.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
